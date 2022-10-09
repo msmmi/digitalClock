c6b1a2e8adce639ae9f3cd99d6508330050fe4be
msmmi <mksmmi40@gmail.com>
2022-10-09 15:52:59 +0600

    final version of clock

---
+++ main.js
@@ -0,0 +1,19 @@
+function getCurrentTime() {
+  const date = new Date();
+  let hours = date.getHours();
+  hours = hours > 12 ? hours - 12 : '0' + hours;
+  const minutes = date.getMinutes();
+  const formatedMinutes = minutes >= 10 ? minutes : '0' + minutes;
+  const seconds = date.getSeconds();
+  const formatedseconds = seconds >= 10 ? seconds : '0' + seconds;
+  return `${hours}:${formatedMinutes}:${formatedseconds}`;
+}
+console.log(getCurrentTime());
+let time = '';
+// setInterval(() => {
+//   time = getCurrentTime();
+// }, 1000);
+const selectH1 = document.querySelector('h1');
+setInterval(() => {
+  selectH1.textContent = getCurrentTime();
+}, 1000);
