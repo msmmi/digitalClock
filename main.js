dae4976e5b04fce592ae1ea15aff0b3d560aa1f7
msmmi <mksmmi40@gmail.com>
2022-10-09 19:31:30 +0600

    final version 1.2

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ function getCurrentTime() {
   const formatedMinutes = minutes >= 10 ? minutes : '0' + minutes;
   const seconds = date.getSeconds();
   const formatedseconds = seconds >= 10 ? seconds : '0' + seconds;
-  return `Hours:Minutes:second: ${hours}:${formatedMinutes}:${formatedseconds}`;
+
+  return date.getHours() > 12
+    ? `Hours:Minutes:second: ${hours}:${formatedMinutes}:${formatedseconds} P.M.`
+    : `Hours:Minutes:second: ${hours}:${formatedMinutes}:${formatedseconds} A.M.`;
 }
 
 const selectH1 = document.querySelector('h1');
